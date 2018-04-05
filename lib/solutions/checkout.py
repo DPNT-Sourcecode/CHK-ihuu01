74df0a8f13a15711198436a63acffa0ad7937c31
sbx_user1059 <sbx_user1059@ip-10-11-66-169.eu-west-2.compute.internal>
2018-04-05 16:04:40 +0000

    Thu Apr 05 16:04:40 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,6 +1,34 @@
 
+ITEMS = {
+    'A': {'price': 50, 'special_offers': (3, 30)},
+    'B': {'price': 30, 'special_offers': (2, 45)},
+    'C': {'price': 20},
+    'D': {'price': 15}
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+
 def checkout(skus):
-    raise NotImplementedError()
+    """
+    Calculate the total amount for the checkout based on the SKUs entered in
+
+    :param skus: string, each char is an item
+    :return: int, total amount of the cart, including special offers
+    """
+    total = 0
+
+    for item in skus:
+        if item in ITEMS:
+            # check for special offers
+            # if 'special_offers' in ITEMS[item]:
+            #     quantity_required,
+
+            # increment the total
+            total += ITEMS[item]['price']
+
+        else:
+            return -1
+
+    return total
