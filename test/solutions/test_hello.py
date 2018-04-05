8c9ad90e492bc6a6d770ec0f2944ef8f9f7bc6fd
sbx_user1059 <sbx_user1059@ip-10-11-66-169.eu-west-2.compute.internal>
2018-04-05 15:43:25 +0000

    Thu Apr 05 15:43:25 UTC 2018

--- test/solutions/test_hello.py
+++ test/solutions/test_hello.py
@@ -5,7 +5,7 @@ from lib.solutions.hello import hello
 
 class TestHello(unittest.TestCase):
     def test_hello(self):
-        self.assertEqual(hello('Steve'), 'Hello, World!')
+        self.assertEqual(hello('John'), 'Hello, John!')
 
 
 if __name__ == '__main__':
